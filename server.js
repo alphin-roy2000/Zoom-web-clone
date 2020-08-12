67e704133f124c0a25d9bf40b26e0a81c81cad77
Alphin Roy <alphinroy430@gmail.com>
2020-08-13 03:10:43 +0530

    base ok creating room id

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const { v4: uuidv4 } = require('uuid');
+app.set('view engine','ejs');
+
+app.get('/',(req ,res)=>{
+    // res.status(200).send("Hello world")
+    // res.render('room');
+    res.redirect(`/${uuidv4()}`)
+})
+
+app.get('/:room',(req,res)=>{
+    res.render('room',{roomId: req.room});
+})
+
+
+server.listen(3030);
