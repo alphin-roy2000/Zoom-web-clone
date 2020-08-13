cc46cb8f0c8a1a0c43cee38b89d9ebaef07b7bd0
Alphin Roy <alphinroy430@gmail.com>
2020-08-14 03:03:54 +0530

    peer some error

--- public/script.js
+++ public/script.js
@@ -1,9 +1,15 @@
-
+const socket = io('/'); 
 const videoGrid = document.getElementById('video-grid')
 console.log(videoGrid);
 const myVideo = document.createElement('video');
 
 myVideo.muted = true;
+
+var peer = new Peer(undefined,{
+  path: '/peerjs',
+  host: '/',
+  port: '3030'
+});
 
 let myVideoStream
 navigator.mediaDevices.getUserMedia({
@@ -12,14 +18,57 @@ navigator.mediaDevices.getUserMedia({
 }).then(stream => {
   myVideoStream = stream;
   addVideoStream(myVideo, stream);
+  
 
-})
-
-const addVideoStream = (video,stream)=> {
-  video.srcObject = stream;
-  video.addEventListener('loadedmetadata',()=> {
-      video.play();
+  // peer.on('call', call => {
+  //   call.answer(stream)
+  //   const video = document.createElement('video')
+  //   call.on('stream', userVideoStream => {
+  //     addVideoStream(video, userVideoStream)
+  //   })
+  // })
+  
+  socket.on('user-connected',(userId)=>{
+    connectToNewUser(userId, stream);
   })
-  videoGrid.append(video);
+});
+
+peer.on('open',id => {
+  // console.log(id);
+  socket.emit('join-room', ROOM_ID, id);
+});
+
+// socket.emit('join-room',ROOM_ID);
+
+// socket.on('user-connected',(userId)=>{
+//   connectToNewUser(userId,stream );
+// })
+
+const connectToNewUser = (userId, stream) => {
+  // console.log('new user');
+  
+  // console.log(userId);
+  const call = peer.call(userId,stream);
+  const video = document .createElement('video');
+  call.on('stream',userVideoStream => {
+    addVideoStream(video, userVideoStream)
+  })
 
 }
+
+const addVideoStream =(video, stream)=> {
+  video.srcObject = stream 
+  video.addEventListener('loadedmetadata',()=> {
+    video.play()
+  })
+  videoGrid.append(video)
+}
+
+// const addVideoStream = (video,stream)=> {
+//   video.srcObject = stream;
+//   video.addEventListener('loadedmetadata',()=> {
+//       video.play();
+//   })
+//   videoGrid.append(video);
+
+// }
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const io =require('socket.io')(server);
 const { v4: uuidv4 } = require('uuid');
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+});
 app.set('view engine','ejs');
-app.use(express.static('public'))
+app.use(express.static('public'));//adding a folder as a global
+
+app.use('/peerjs',peerServer);
 
 app.get('/',(req ,res)=>{
     // res.status(200).send("Hello world")
@@ -17,5 +24,12 @@ app.get('/:room',(req,res)=>{
 
 })
 
+io.on('connection',socket => {
+    socket.on('join-room',(roomId,userId)=>{
+        // console.log('joined room');
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit('user-connected',userId);
+    })
+})
 
 server.listen(3030);
